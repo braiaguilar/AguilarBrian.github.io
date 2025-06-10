3836dd079df2048ad7a165e2c65f767f81e95740
braiaguilar <brianisaiasaguilar@gmail.com>
2025-06-10 10:51:26 -0300

    20.06.10 Change pfp

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default class About extends Component {
 
             <div className="three columns" id="profile-div">
 
-               <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
+               <img className="profile-pic"  src="images/profilepic.png" alt="" />
 
             </div>
 
